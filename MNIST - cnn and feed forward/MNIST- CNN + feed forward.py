d549e0a315f795556a6c106c5dba28d535b7c96f
Hitz_machine <hitesh@prescienceds.com>
2020-12-06 09:07:36 +0530

    Create MNIST- CNN + feed forward.py

---
+++ MNIST - cnn and feed forward/MNIST- CNN + feed forward.py
@@ -0,0 +1,229 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  6 08:58:41 2020
+
+@author: HiteshNayak
+"""
+
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from torch.autograd import Variable
+
+# Specify the Mean and standard deviation of all the pixels in the MNIST dataset. They are precomputed 
+mean_gray = 0.1307
+stddev_gray = 0.3081
+
+#Transform the images to tensors
+#Normalize a tensor image with mean and standard deviation. Given mean: (M1,...,Mn) and std: (S1,..,Sn) 
+#for n channels, this transform will normalize each channel of the input torch.Tensor
+#i.e. input[channel] = (input[channel] - mean[channel]) / std[channel]
+
+transforms = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((mean_gray,), (stddev_gray,))])
+
+#Load our dataset
+train_dataset = datasets.MNIST(root = './data', 
+                            train = True, 
+                            transform = transforms,
+                            download = True)
+
+test_dataset = datasets.MNIST(root = './data', 
+                            train = False, 
+                            transform = transforms)
+
+import matplotlib.pyplot as plt
+random_image = train_dataset[20][0].numpy() * stddev_gray + mean_gray
+plt.imshow(random_image.reshape(28, 28), cmap='gray')
+
+print(train_dataset[20][1])   #Print the corresponding label for the image
+
+#Make the dataset iterable
+batch_size = 100
+
+train_load = torch.utils.data.DataLoader(dataset = train_dataset, 
+                                         batch_size = batch_size,
+                                         shuffle = True)
+
+test_load = torch.utils.data.DataLoader(dataset = test_dataset, 
+                                         batch_size = batch_size,
+                                         shuffle = False)
+
+print('There are {} images in the training set'.format(len(train_dataset)))
+print('There are {} images in the test set'.format(len(test_dataset)))
+print('There are {} batches in the train loader'.format(len(train_load)))
+print('There are {} batches in the testloader'.format(len(test_load)))
+
+#Create the model class
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN,self).__init__()
+        #Same Padding = [(filter size - 1) / 2] (Same Padding--> input size = output size)
+        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3,stride=1, padding=1)
+        #The output size of each of the 8 feature maps is 
+        #[(input_size - filter_size + 2(padding) / stride) +1] --> [(28-3+2(1)/1)+1] = 28 (padding type is same)
+        #Batch normalization
+        self.batchnorm1 = nn.BatchNorm2d(8)
+        #RELU
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=2)
+        #After max pooling, the output of each feature map is now 28/2 = 14
+        self.cnn2 = nn.Conv2d(in_channels=8, out_channels=32, kernel_size=5, stride=1, padding=2)
+        #Output size of each of the 32 feature maps remains 14
+        self.batchnorm2 = nn.BatchNorm2d(32)
+        #After max pooling, the output of each feature map is 14/2 = 7
+        #Flatten the feature maps. You have 32 feature maps, each of them is of size 7x7 --> 32*7*7 = 1568
+        self.fc1 = nn.Linear(in_features=1568, out_features=600)
+        self.droput = nn.Dropout(p=0.5)
+        self.fc2 = nn.Linear(in_features=600, out_features=10)
+    def forward(self,x):
+        out = self.cnn1(x)
+        out = self.batchnorm1(out)
+        out = self.relu(out)
+        out = self.maxpool(out)
+        out = self.cnn2(out)
+        out = self.batchnorm2(out)
+        out = self.relu(out)
+        out = self.maxpool(out)
+        #Now we have to flatten the output. This is where we apply the feed forward neural network as learned before! 
+        #It will take the shape (batch_size, 1568) = (100, 1568)
+        out = out.view(-1,1568)
+        #Then we forward through our fully connected layer 
+        out = self.fc1(out)
+        out = self.relu(out)
+        out = self.droput(out)
+        out = self.fc2(out)
+        return out
+    
+model = CNN()
+CUDA = torch.cuda.is_available()
+if CUDA:
+    model = model.cuda()    
+loss_fn = nn.CrossEntropyLoss()   
+     
+# Show weight decay
+optimizer = torch.optim.SGD(model.parameters(), lr = 0.01)
+
+iteration = 0
+correct = 0
+
+for i,(inputs,labels) in enumerate (train_load):
+
+    if CUDA:
+        inputs = inputs.cuda()
+        labels = labels.cuda()
+        
+    print("For one iteration, this is what happens:")
+    print("Input Shape:",inputs.shape)
+    print("Labels Shape:",labels.shape)
+    output = model(inputs)
+    print("Outputs Shape",output.shape)
+    _, predicted = torch.max(output, 1)
+    print("Predicted Shape",predicted.shape)
+    print("Predicted Tensor:")
+    print(predicted)
+    correct += (predicted == labels).sum()
+    break
+
+#Training the CNN
+num_epochs = 25
+
+#Define the lists to store the results of loss and accuracy
+train_loss = []
+test_loss = []
+train_accuracy = []
+test_accuracy = []
+
+#Training
+for epoch in range(num_epochs): 
+    #Reset these below variables to 0 at the begining of every epoch
+    correct = 0
+    iterations = 0
+    iter_loss = 0.0
+    
+    model.train()                   # Put the network into training mode
+    
+    for i, (inputs, labels) in enumerate(train_load):
+        
+        if CUDA:
+            inputs = inputs.cuda()
+            labels = labels.cuda()
+        
+        outputs = model(inputs)         
+        loss = loss_fn(outputs, labels)  
+        iter_loss += loss.item()         # Accumulate the loss
+        optimizer.zero_grad()            # Clear off the gradient in (w = w - gradient)
+        loss.backward()                 # Backpropagation 
+        optimizer.step()                # Update the weights
+        
+        # Record the correct predictions for training data 
+        _, predicted = torch.max(outputs, 1)
+        correct += (predicted == labels).sum()
+        iterations += 1
+    
+    # Record the training loss
+    train_loss.append(iter_loss/iterations)
+    # Record the training accuracy
+    train_accuracy.append((100 * correct / len(train_dataset)))
+   
+    #Testing
+    testing_loss = 0.0
+    correct = 0
+    iterations = 0
+
+    model.eval()                    # Put the network into evaluation mode
+    
+    for i, (inputs, labels) in enumerate(test_load):
+
+        if CUDA:
+            inputs = inputs.cuda()
+            labels = labels.cuda()
+        
+        outputs = model(inputs)     
+        loss = loss_fn(outputs, labels) # Calculate the loss
+        testing_loss += loss.item()
+        # Record the correct predictions for training data
+        _, predicted = torch.max(outputs, 1)
+        correct += (predicted == labels).sum()
+        
+        iterations += 1
+
+    # Record the Testing loss
+    test_loss.append(testing_loss/iterations)
+    # Record the Testing accuracy
+    test_accuracy.append((100 * correct / len(test_dataset)))
+    
+    print ('Epoch {}/{}, Training Loss: {:.3f}, Training Accuracy: {:.3f}, Testing Loss: {:.3f}, Testing Acc: {:.3f}'
+           .format(epoch+1, num_epochs, train_loss[-1], train_accuracy[-1], 
+             test_loss[-1], test_accuracy[-1]))
+    
+    
+# Loss
+f = plt.figure(figsize=(10, 10))
+plt.plot(train_loss, label='Training Loss')
+plt.plot(test_loss, label='Testing Loss')
+plt.legend()
+plt.show()
+
+# Accuracy
+f = plt.figure(figsize=(10, 10))
+plt.plot(train_accuracy, label='Training Accuracy')
+plt.plot(test_accuracy, label='Testing Accuracy')
+plt.legend()
+plt.show()
+
+img = test_dataset[30][0].resize_((1, 1, 28, 28))   #(batch_size,channels,height,width)
+label = test_dataset[30][1]
+
+model.eval()
+
+#GPU usage 
+if CUDA:
+    model = model.cuda()
+    img = img.cuda()
+    
+output = model(img)
+_, predicted = torch.max(output,1)
+print("Prediction is: {}".format(predicted.item()))
+print("Actual is: {}".format(label))
